5d85470c5033cd15869b866953b88b2374f3acc2
Tyler Guerricabeitia <hunets4506@gmail.com>
2023-03-09 10:10:09 -0700

    working tabs for admin page

--- client/src/AdminPage.js
+++ client/src/AdminPage.js
@@ -2,6 +2,7 @@ import React from "react";
 import './css/style.css';
 import { Link } from "react-router-dom";
 import CourseModel from "./CourseModel";
+import Tabs from './components/Tabs';
 
 function CreateAdminPage() {
     return(
@@ -11,38 +12,13 @@ function CreateAdminPage() {
                 <Link to={"/Login"}><h3>Log Out</h3></Link>
             </div>
             <div id="adminMain">
-                <div id="adminInfoHeader">
-                    <h1>Admin Information</h1>
-                    <button type="submit">Edit Information</button>
-                </div>
-                <div id="adminInfo">
-                    <div id="info1">
-                        <label>First Name:</label>
-                        <input type="text"></input>
-                        <label>Last Name:</label>
-                        <input type="text"></input>
-                        <label>User Name:</label>
-                        <input type="text"></input>
-                    </div>
-                    <div id="info2">
-                        <label>Phone Number:</label>
-                        <input type="number"></input>
-                        <label>Address:</label>
-                        <input type="text"></input>
-                        <label>Email:</label>
-                        <input type="text"></input>
-                    </div>
-                </div>
-                <div id="searchBar">
-                    <label>Search Student:</label>
-                    <input type="text"></input>
-                </div>
-                <div id="studentInfoLocation">
-                    <div id="studentInfoHeader">
-                        <h1>Student Information</h1>
+            <Tabs>
+                <div label="Admin Information">
+                    <div id="adminInfoHeader">
+                        <h1>Admin Information</h1>
                         <button type="submit">Edit Information</button>
                     </div>
-                    <div id="studentInfo">
+                    <div id="adminInfo">
                         <div id="info1">
                             <label>First Name:</label>
                             <input type="text"></input>
@@ -60,14 +36,46 @@ function CreateAdminPage() {
                             <input type="text"></input>
                         </div>
                     </div>
-                    <div id="studentScheduleHeader">
-                        <h1>Student Schedule</h1>
-                        <button type="submit">Submit Changes</button>
+                </div>
+                {/* /// */}
+                <div label="Student Information">
+                    <div id="searchBar">
+                        <label>Search Student:</label>
+                        <input type="text"></input>
                     </div>
-                    <div id="studentScheduleLocation">
-                        <CourseModel />
+                    <div id="studentInfoLocation">
+                        <div id="studentInfoHeader">
+                            <h1>Student Information</h1>
+                            <button type="submit">Edit Information</button>
+                        </div>
+                        <div id="studentInfo">
+                            <div id="info1">
+                                <label>First Name:</label>
+                                <input type="text"></input>
+                                <label>Last Name:</label>
+                                <input type="text"></input>
+                                <label>User Name:</label>
+                                <input type="text"></input>
+                            </div>
+                            <div id="info2">
+                                <label>Phone Number:</label>
+                                <input type="number"></input>
+                                <label>Address:</label>
+                                <input type="text"></input>
+                                <label>Email:</label>
+                                <input type="text"></input>
+                            </div>
+                        </div>
+                        <div id="studentScheduleHeader">
+                            <h1>Student Schedule</h1>
+                            <button type="submit">Submit Changes</button>
+                        </div>
+                        <div id="studentScheduleLocation">
+                            <CourseModel />
+                        </div>
                     </div>
                 </div>
+            </Tabs>
             </div>
         </div>
     )
