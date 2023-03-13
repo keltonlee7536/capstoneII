613e7ee4a3fa70e1b50f1adc6eb5cbb3da61203a
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2023-03-13 10:22:29 -0600

    test

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,8 +1,42 @@
 import './css/style.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
+function auth(users){
+    const auth_username = document.getElementById('username').innerHTML
+    const auth_password = document.getElementById('user_password').innerHTML
+
+    if (auth_username === users.username && auth_password === users.password){
+    const authRoute = "/studenthome"
+    console.log("authenticated" + authRoute)
+
+    }else{
+        let authRoute = "/login"
+        window.alert("not valid username or password")
+        console.log("authentication failed" + authRoute)
+    }
+}
+
 function CreateLogin() {
+
+    //fetchging the data
+const[users, setUsers] = useState()
+
+const fetchData = () => {
+    fetch("/getuser")
+    .then(response => {
+        return response.json()
+    }).then(data => {
+        console.log(data.users[0]);
+        setUsers(data.users[0])
+}).catch(console.error)}
+
+useEffect(() => {
+    fetchData()
+}, []);
+
+    const authRoute = "/SignUp"
+
     return (
         <div className='loginOverall'>
             <div className='loginContainer'>
@@ -10,11 +44,12 @@ function CreateLogin() {
                     <h1>Login</h1>
                 </div>
                 <div className='credentials'>
-                    <label>Email:</label>
-                    <input type="text" id="user_email" required></input>
+                    <label>Username or Email:</label>
+                    <input type="text" id="username" required></input>
                     <label>Password:</label>
                     <input type="text" id="user_password" required></input>
-                    <Link to ={'/StudentHome'}><button type="submit" href="/Home">Login</button></Link>
+                    <Link to ={authRoute}><button onClick = {auth()} type="submit" href="/Home">Login</button></Link>
+
                 </div>
                 <div className='signUp'>
                     <p>Don't have an account? <Link to={'/SignUp'}>Sign Up</Link>
